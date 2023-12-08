224b5f9c1425d0829e538bdd12cef35aff319a9d
Son Giang <sgiangt@gmail.com>
2023-12-07 18:24:35 -0700

    updated models and schema to remove game, genre, orders, and wishlist as well as stripe

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,3 @@
 const User = require('./User');
-const Game = require('./Game');
-const Genre = require('./Genre');
-const Order = require('./Order');
-const Wishlist = require('./Wishlist'); 
 
-module.exports = { User, Game, Genre, Order, Wishlist };
+module.exports = { User };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,41 +1,9 @@
 const typeDefs = `
-  type Genre {
-    _id: ID
-    name: String
-  }
-
-  type Game {
-    _id: ID
-    name: String
-    description: String
-    image: String
-    quantity: Int
-    price: Float
-    genre: Genre
-  }
-
-  type Wishlist {
-    _id: ID
-    user: User
-    games: [Game]
-  }
-
-  type Order {
-    _id: ID
-    purchaseDate: String
-    games: [Game]
-  }
-
   type User {
     _id: ID
     firstName: String
     lastName: String
     email: String
-    orders: [Order]
-  }
-
-  type Checkout {
-    session: ID
   }
 
   type Auth {
@@ -43,34 +11,16 @@ const typeDefs = `
     user: User
   }
 
-  input GameInput {
-    _id: ID
-    purchaseQuantity: Int
-    name: String
-    image: String
-    price: Float
-    quantity: Int
-  }
-
   type Query {
-    genres: [Genre]
-    games(genre: ID, name: String): [Game]
-    game(_id: ID!): Game
     user: User
-    order(_id: ID!): Order
-    wishlist: Wishlist 
-    checkout(games: [GameInput]): Checkout
   }
 
   type Mutation {
     addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
-    addOrder(games: [ID]!): Order
     updateUser(firstName: String, lastName: String, email: String, password: String): User
-    updateGame(_id: ID!, quantity: Int!): Game
     login(email: String!, password: String!): Auth
-    addToWishlist(gameId: ID!): Wishlist
-    removeFromWishlist(gameId: ID!): Wishlist 
   }
 `;
 
 module.exports = typeDefs;
+
